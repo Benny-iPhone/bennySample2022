b24f639c10ea7a10f0dabea682e3d07801af7710
Benny <benny@bdapps.net>
2022-05-17 20:02:21 +0300

    Setup Xcode Project

---
+++ FinalProject/FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Benny Davidovitz on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
