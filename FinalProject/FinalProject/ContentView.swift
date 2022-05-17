e3f883105c30888634e1dfa676f74415d2cb9a19
Benny <benny@bdapps.net>
2022-05-17 20:08:31 +0300

    some message

--- FinalProject/FinalProject/ContentView.swift
+++ FinalProject/FinalProject/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+// a change
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, final project!")
---
+++ FinalProject/FinalProject/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  FinalProject
+//
+//  Created by Benny Davidovitz on 17/05/2022.
+//
+
+import Foundation
